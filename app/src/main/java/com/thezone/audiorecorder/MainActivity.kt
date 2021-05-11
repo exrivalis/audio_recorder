5130c12add706442789a2d9a8938cc63968e145d
exrivalis <saoudet.ahmed@gmail.com>
2021-05-11 15:55:38 +0200

    #2: permission

---
+++ app/src/main/java/com/thezone/audiorecorder/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.thezone.audiorecorder
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val REQUEST_CODE = 200
+
+class MainActivity : AppCompatActivity() {
+
+    private var permissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+    private var permissionGranted = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        permissionGranted = ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED
+
+        if(!permissionGranted)
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+
+
+        btnRecord.setOnClickListener {
+            startRecording()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == REQUEST_CODE)
+            permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startRecording(){
+        if(!permissionGranted){
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+            return
+        }
+        // start recording
+    }
+}
