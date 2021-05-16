29d6ab0bb33b5ccd273a67630e8cba58eec50141
exrivalis <saoudet.ahmed@gmail.com>
2021-05-16 13:20:19 +0200

    #3 MediaRecorder to mp3 file

--- app/src/main/java/com/thezone/audiorecorder/MainActivity.kt
+++ app/src/main/java/com/thezone/audiorecorder/MainActivity.kt
@@ -2,10 +2,14 @@ package com.thezone.audiorecorder
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 const val REQUEST_CODE = 200
 
@@ -13,6 +17,12 @@ class MainActivity : AppCompatActivity() {
 
     private var permissions = arrayOf(Manifest.permission.RECORD_AUDIO)
     private var permissionGranted = false
+
+    private lateinit var recorder: MediaRecorder
+    private var dirPath = ""
+    private var filename = ""
+    private var isRecording = false
+    private var isPaused = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +35,11 @@ class MainActivity : AppCompatActivity() {
 
 
         btnRecord.setOnClickListener {
-            startRecording()
+            when{
+                isPaused -> resumeRecorder()
+                isRecording -> pauseRecorder()
+                else -> startRecording()
+            }
         }
     }
 
@@ -40,11 +54,47 @@ class MainActivity : AppCompatActivity() {
             permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
     }
 
+    private fun pauseRecorder(){
+        recorder.pause()
+        isPaused = true
+        btnRecord.setImageResource(R.drawable.ic_record)
+    }
+
+    private fun resumeRecorder(){
+        recorder.resume()
+        isPaused = false
+        btnRecord.setImageResource(R.drawable.ic_pause)
+    }
+
     private fun startRecording(){
         if(!permissionGranted){
             ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
             return
         }
         // start recording
+
+        recorder = MediaRecorder()
+        dirPath = "${externalCacheDir?.absolutePath}/"
+
+        var simpleDateFormat = SimpleDateFormat("yyyy.MM.DD_hh.mm.ss")
+        var date = simpleDateFormat.format(Date())
+        filename = "audio_record_$date"
+
+        recorder.apply{
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+            setOutputFile("$dirPath$filename.mp3")
+
+            try {
+                prepare()
+            }catch (e: IOException){}
+
+            start()
+        }
+
+        btnRecord.setImageResource(R.drawable.ic_pause)
+        isRecording = true
+        isPaused = false
     }
 }
