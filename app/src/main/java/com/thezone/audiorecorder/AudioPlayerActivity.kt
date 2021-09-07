1cf37bf85f79a9b8c5baba477510b48bb0fa5c5c
exrivalis <saoudet.ahmed@gmail.com>
2021-09-07 09:04:16 +0200

    AudioPlayerActivity: layout setup
    Former-commit-id: 8758f51c7532a24336ebbfc27a163ed905d5ba7e

---
+++ app/src/main/java/com/thezone/audiorecorder/AudioPlayerActivity.kt
@@ -0,0 +1,11 @@
+package com.thezone.audiorecorder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AudioPlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audio_player)
+    }
+}
