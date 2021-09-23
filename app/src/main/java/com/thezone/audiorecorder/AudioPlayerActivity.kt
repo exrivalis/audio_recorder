43b0ba26988b1052a0b70014452d9d208fb00ed6
exrivalis <saoudet.ahmed@gmail.com>
2021-09-24 00:46:17 +0200

    #19 play audio pt.2
    Former-commit-id: f364bfb407093e94a70d4e329cc464fd3a52da2b

--- app/src/main/java/com/thezone/audiorecorder/AudioPlayerActivity.kt
+++ app/src/main/java/com/thezone/audiorecorder/AudioPlayerActivity.kt
@@ -1,11 +1,118 @@
 package com.thezone.audiorecorder
 
+import android.media.MediaPlayer
+import android.media.PlaybackParams
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageButton
+import android.widget.SeekBar
+import androidx.core.content.res.ResourcesCompat
+import com.google.android.material.chip.Chip
+import kotlinx.android.synthetic.main.activity_audio_player.*
 
 class AudioPlayerActivity : AppCompatActivity() {
+    private lateinit var mediaPlayer: MediaPlayer
+
+    private lateinit var btnPlay : ImageButton
+    private lateinit var btnBackward : ImageButton
+    private lateinit var btnForward : ImageButton
+    private lateinit var speedChip : Chip
+    private lateinit var seekBar : SeekBar
+
+    private lateinit var runnable: Runnable
+    private lateinit var handler: Handler
+    private var delay = 1000L
+    private var jumpValue = 1000
+
+    private var playbackSpeed = 1.0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_audio_player)
+
+        var filePath = intent.getStringExtra("filepath")
+        var fileName = intent.getStringExtra("filename")
+
+
+        mediaPlayer = MediaPlayer()
+        mediaPlayer.apply {
+            setDataSource(filePath)
+            prepare()
+        }
+
+
+        btnBackward = findViewById(R.id.btnBackward)
+        btnForward = findViewById(R.id.btnForward)
+        btnPlay = findViewById(R.id.btnPlay)
+        speedChip = findViewById(R.id.chip)
+        seekBar = findViewById(R.id.seekBar)
+
+
+        handler = Handler(Looper.getMainLooper())
+        runnable = Runnable {
+            seekBar.progress = mediaPlayer.currentPosition
+            handler.postDelayed(runnable, delay)
+        }
+
+        btnPlay.setOnClickListener {
+            playPausePlayer()
+        }
+
+        playPausePlayer()
+        seekBar.max = mediaPlayer.duration
+
+        mediaPlayer.setOnCompletionListener {
+            btnPlay.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_play_circle, theme)
+            handler.removeCallbacks(runnable)
+        }
+
+
+        btnForward.setOnClickListener {
+            mediaPlayer.seekTo(mediaPlayer.currentPosition + jumpValue)
+            seekBar.progress += jumpValue
+        }
+
+        btnBackward.setOnClickListener {
+            mediaPlayer.seekTo(mediaPlayer.currentPosition - jumpValue)
+            seekBar.progress -= jumpValue
+        }
+
+        chip.setOnClickListener {
+            if(playbackSpeed != 2f)
+                playbackSpeed += 0.5f
+            else
+                playbackSpeed = 0.5f
+
+            mediaPlayer.playbackParams = PlaybackParams().setSpeed(playbackSpeed)
+            chip.text = "x $playbackSpeed"
+        }
+
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                if(p2)
+                    mediaPlayer.seekTo(p1)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+        })
+    }
+
+
+    private fun playPausePlayer(){
+        if(!mediaPlayer.isPlaying){
+            mediaPlayer.start()
+            btnPlay.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_pause_circle, theme)
+            handler.postDelayed(runnable, delay)
+        }else{
+            mediaPlayer.pause()
+            btnPlay.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_play_circle, theme)
+            handler.removeCallbacks(runnable)
+        }
     }
 }
--- app/src/main/java/com/thezone/audiorecorder/GalleryActivity.kt
+++ app/src/main/java/com/thezone/audiorecorder/GalleryActivity.kt
@@ -1,5 +1,6 @@
 package com.thezone.audiorecorder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -49,7 +50,12 @@ class GalleryActivity : AppCompatActivity(), OnItemClickListener {
     }
 
     override fun onItemClickListener(position: Int) {
-        Toast.makeText(this, "Simple click", Toast.LENGTH_SHORT).show()
+        var audioRecord = records[position]
+        var intent = Intent(this, AudioPlayerActivity::class.java)
+
+        intent.putExtra("filepath", audioRecord.filePath)
+        intent.putExtra("filename", audioRecord.filename)
+        startActivity(intent)
     }
 
     override fun onItemLongClickListener(position: Int) {
